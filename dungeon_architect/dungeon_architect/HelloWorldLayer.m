c1a33a3a11c18df820dd3a6f0b7a1500d8c393a5
Andre May <amay@macbook-pro-andre.fritz.box>
2012-11-03 18:10:45 +0100

    The Priest entered the game

--- dungeon_architect/dungeon_architect/HelloWorldLayer.m
+++ dungeon_architect/dungeon_architect/HelloWorldLayer.m
@@ -34,6 +34,56 @@
 	return scene;
 }
 
+-(void)spriteMoveFinished:(id)sender {
+    CCSprite *sprite = (CCSprite *)sender;
+    [self removeChild:sprite cleanup:YES];
+}
+
+-(void)moveBarbarian {
+    
+    // Zufallszahl erzeugen
+    int randomInt;
+    randomInt = (arc4random() % 2) + 1;
+    CCSprite *target = [CCSprite spriteWithFile:@"barbarian.png"
+                                           rect:CGRectMake(0, 0, 35, 35)];
+    
+    if(randomInt == 1){
+        target = [CCSprite spriteWithFile:@"barbarian.png"
+                                               rect:CGRectMake(0, 0, 35, 35)];
+    }
+    else {
+        target = [CCSprite spriteWithFile:@"priest.png"
+                                               rect:CGRectMake(0, 0, 35, 35)];
+    }
+    
+    
+    // Determine where to spawn the target along the Y axis
+    CGSize winSize = [[CCDirector sharedDirector] winSize];
+    int minY = target.contentSize.height/2;
+    int maxY = winSize.height - target.contentSize.height/2;
+    int rangeY = maxY - minY;
+    int actualY = (arc4random() % rangeY) + minY;
+    
+    // Create the target slightly off-screen along the right edge,
+    // and along a random position along the Y axis as calculated above
+    target.position = ccp(winSize.width + (target.contentSize.width/2), actualY);
+    [self addChild:target];
+    
+    // Determine speed of the target
+    int minDuration = 1.0;
+    int maxDuration = 20.0;
+    int rangeDuration = maxDuration - minDuration;
+    int actualDuration = (arc4random() % rangeDuration) + minDuration;
+    
+    // Create the actions
+    id actionMove = [CCMoveTo actionWithDuration:actualDuration
+                                        position:ccp(-target.contentSize.width/2, actualY)];
+    id actionMoveDone = [CCCallFuncN actionWithTarget:self
+                                             selector:@selector(spriteMoveFinished:)];
+    [target runAction:[CCSequence actions:actionMove, actionMoveDone, nil]];
+    
+}
+
 // on "init" you need to initialize your instance
 -(id) init
 {
@@ -108,9 +158,16 @@
 		
 		// Add the menu to the layer
 		[self addChild:menu];
+        
+        // Add Schedule to move babarian
+        [self schedule:@selector(gameLogic:) interval:1.0];
 
 	}
 	return self;
+}
+
+-(void)gameLogic:(ccTime)dt {
+    [self moveBarbarian];
 }
 
 // on "dealloc" you need to release all your retained objects
