0d526b7e8b307ef60547d9ded8cd34d598f90872
Christian May <chrisapril@Christians-MacBook-Air.local>
2012-11-04 22:18:03 +0100

    Einbau Anfänge dynamisches setzen von tiles

--- dungeon_architect/dungeon_architect/GameScene.m
+++ dungeon_architect/dungeon_architect/GameScene.m
@@ -5,6 +5,8 @@
 //  Created by Christian May on 04.11.12.
 //
 //
+//http://www.raywenderlich.com/1163/how-to-make-a-tile-based-game-with-cocos2d
+//
 
 
 
@@ -20,7 +22,11 @@
 
 //http://stackoverflow.com/questions/538996/constants-in-objective-c
 // Constants.m
-NSString *const TILEMAP = @"background_tilemap.tmx";
+NSString *const TILEMAP = @"tilemap.tmx";
+
+NSString *const PLAYER = @"sage.gif";
+
+
 
 +(id) scene
 {
@@ -61,22 +67,26 @@ NSString *const TILEMAP = @"background_tilemap.tmx";
 	if( (self=[super init] )) {
         
         self.isTouchEnabled = YES;
-        //self.tileMap = [CCTMXTiledMap tiledMapWithTMXFile:@"TileMap.tmx"];
         self.tileMap = [CCTMXTiledMap tiledMapWithTMXFile:TILEMAP];
         self.background = [_tileMap layerNamed:@"Background"];
         
-//        CCTMXObjectGroup *objects = [_tileMap objectGroupNamed:@"Objects"];
-//        NSAssert(objects != nil, @"'Objects' object group not found");
-//        NSMutableDictionary *spawnPoint = [objects objectNamed:@"SpawnPoint"];
-//        NSAssert(spawnPoint != nil, @"SpawnPoint object not found");
-//        int x = [[spawnPoint valueForKey:@"x"] intValue];
-//        int y = [[spawnPoint valueForKey:@"y"] intValue];
-//        
-//        self.player = [CCSprite spriteWithFile:@"Player.png"];
-//        _player.position = ccp(x, y);
-//        [self addChild:_player];
-//        
-//        [self setViewpointCenter:_player.position];
+        //        CCTMXObjectGroup *objects = [_tileMap objectGroupNamed:@"Objects"];
+        //        NSAssert(objects != nil, @"'Objects' object group not found");
+        //        NSMutableDictionary *spawnPoint = [objects objectNamed:@"SpawnPoint"];
+        //        NSAssert(spawnPoint != nil, @"SpawnPoint object not found");
+        //        int x = [[spawnPoint valueForKey:@"x"] intValue];
+        //        int y = [[spawnPoint valueForKey:@"y"] intValue];
+        //
+        
+        int x =0;
+        int y = 0;
+        
+        
+        self.player = [CCSprite spriteWithFile:PLAYER];
+        _player.position = ccp(x, y);
+        [self addChild:_player];
+        
+        [self setViewpointCenter:_player.position];
         
         [self addChild:_tileMap z:-1];
         
@@ -93,6 +103,14 @@ NSString *const TILEMAP = @"background_tilemap.tmx";
 -(BOOL) ccTouchBegan:(UITouch *)touch withEvent:(UIEvent *)event
 {
     CCLOG(@"ccTouchBegan");
+    
+    CGPoint touchLocation = [touch locationInView: [touch view]];
+    touchLocation = [[CCDirector sharedDirector] convertToGL: touchLocation];
+    touchLocation = [self convertToNodeSpace:touchLocation];
+    
+    
+    [self setPlayerPosition:touchLocation];
+    
 	return YES;
 }
 
@@ -109,32 +127,74 @@ NSString *const TILEMAP = @"background_tilemap.tmx";
     touchLocation = [[CCDirector sharedDirector] convertToGL: touchLocation];
     touchLocation = [self convertToNodeSpace:touchLocation];
     
-    CGPoint playerPos = _player.position;
-    CGPoint diff = ccpSub(touchLocation, playerPos);
-    if (abs(diff.x) > abs(diff.y)) {
-        if (diff.x > 0) {
-            playerPos.x += _tileMap.tileSize.width;
-        } else {
-            playerPos.x -= _tileMap.tileSize.width;
-        }
-    } else {
-        if (diff.y > 0) {
-            playerPos.y += _tileMap.tileSize.height;
-        } else {
-            playerPos.y -= _tileMap.tileSize.height;
-        }
-    }
+    
+    //!!!!code kann man nutzen um tile
+    //    CGPoint playerPos = _player.position;
+//    CGPoint diff = ccpSub(touchLocation, playerPos);
+//    if (abs(diff.x) > abs(diff.y)) {
+//        if (diff.x > 0) {
+//            playerPos.x += _tileMap.tileSize.width;
+//        } else {
+//            playerPos.x -= _tileMap.tileSize.width;
+//        }
+//    } else {
+//        if (diff.y > 0) {
+//            playerPos.y += _tileMap.tileSize.height;
+//        } else {
+//            playerPos.y -= _tileMap.tileSize.height;
+//        }
+//    }
     //player.position = playerPos; // Todo: Trymove
+//    
+//    if (playerPos.x <= (_tileMap.mapSize.width * _tileMap.tileSize.width) &&
+//        playerPos.y <= (_tileMap.mapSize.height * _tileMap.tileSize.height) &&
+//        playerPos.y >= 0 &&
+//        playerPos.x >= 0 ) {
+//        [self setPlayerPosition:playerPos];
+//    }
+//    
+//    [self setViewpointCenter:_player.position];
     
-    if (playerPos.x <= (_tileMap.mapSize.width * _tileMap.tileSize.width) &&
-        playerPos.y <= (_tileMap.mapSize.height * _tileMap.tileSize.height) &&
-        playerPos.y >= 0 &&
-        playerPos.x >= 0 ) {
-        [self setPlayerPosition:playerPos];
-    }
+    [self setPlayerPosition:touchLocation];
     
-    [self setViewpointCenter:_player.position];
     
+    CCTMXLayer* backgroundLayer = [self.tileMap layerNamed:@"background"];
+    
+    
+    unsigned int gid1 = [backgroundLayer tileGIDAt:ccp(1,50)];
+    
+    unsigned int gid = [backgroundLayer tileGIDAt:ccp(10,50)];
+    
+    
+    CCTMXLayer* layer = [self.tileMap layerNamed:@"Kachelebene 2"];
+    
+    //todo positiin touch auf kachel positionen mappen
+    [backgroundLayer setTileGID: 4 at: ccp(10, 50)];
+    
+    
+    
+}
+
+
+//TODO: man darf nicht ausserhalb der map zoomen, code von ccTouchedEnd nehmen, um das zu verhindern
+//Methode ermöglicht das navigiere auf der karte indem man ein touch macht und die karte hin  und her zieht
+-(void) ccTouchMoved:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    
+    CCLOG(@"ccTouchMoved");
+    
+    CGPoint touchLocation = [touch locationInView: [touch view]];
+    CGPoint prevLocation = [touch previousLocationInView: [touch view]];
+    
+    touchLocation = [[CCDirector sharedDirector] convertToGL: touchLocation];
+    prevLocation = [[CCDirector sharedDirector] convertToGL: prevLocation];
+    
+    CGPoint diff = ccpSub(touchLocation,prevLocation);
+    
+    
+    [self setPosition: ccpAdd([self position], diff)];
+    [self setPlayerPosition:touchLocation];
+
 }
 
 // on "dealloc" you need to release all your retained objects
